045af09b5308e4068ab22c6dc1c8be7aee452581
ochecodes7 <ochecodes@gmail.com>
2023-01-04 18:57:56 +0100

    bootstrap and routes Activated

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,28 @@
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/home';
+// Import the Navbar, Nav and Container components from Bootstrap for a nice layout
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
 
 function App() {
   return (
+    <>
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand href="/">My Blog</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Nav className="me-auto">
+            <Nav.Link href="/posts/new">New</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
+    </>
   );
 }
 
